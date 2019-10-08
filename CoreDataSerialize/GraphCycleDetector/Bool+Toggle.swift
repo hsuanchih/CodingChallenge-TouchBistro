7511f7b957cacb16b8621fa555caf6208018fd73
Hsuan-Chih Chuang <hsuanchih.chuang@gmail.com>
2019-10-08 13:36:21 -0400

    Add protocol GraphCycleDetector and convenience methods. Object representations in the given core data model are intentionally cyclic, facilitating a complete object graph traversal regardless of starting point. Conformance to GraphicCycleDetector allows objects currently on the traversal stack to be marked as "visiting", thus preventing cyclic visits.

---
+++ CoreDataSerialize/GraphCycleDetector/Bool+Toggle.swift
@@ -0,0 +1,14 @@
+//
+//  Bool+Toggle.swift
+//  CoreDataSerialize
+//
+//  Created by Hsuan-Chih Chuang on 2019/10/8.
+//
+
+import Foundation
+
+extension Bool {
+    mutating func toggle() {
+        self = !self
+    }
+}
---
+++ CoreDataSerialize/GraphCycleDetector/GraphCycleDetector.swift
@@ -0,0 +1,12 @@
+//
+//  GraphCycleBreaker.swift
+//  CoreDataSerialize
+//
+//  Created by Hsuan-Chih Chuang on 2019/10/8.
+//
+
+import Foundation
+
+protocol GraphCycleDetector {
+    var isVisiting : Bool { get set }
+}
